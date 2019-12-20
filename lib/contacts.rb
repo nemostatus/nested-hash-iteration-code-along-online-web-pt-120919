f7a6dec87aeb1589b807e26829a8a17c26896062
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-20 09:43:11 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 
   # This is the array we will be passing into the remove_strawberry method
    contacts = {
@@ -13,10 +13,10 @@ require 'pry'
        favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
      }
   }
-end
+
   
 def remove_strawberry(contacts)
 contacts.each do |person, contact_details_hash|
-  binding pry
+  
 end
 
