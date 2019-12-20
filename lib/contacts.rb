18054b2983cd5e366f6ba9ef5bd3cd12f77b82f1
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-20 09:30:47 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -13,6 +13,7 @@ require 'pry'
        favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
      }
    }
+ end
   
 def remove_strawberry(contacts)
 contacts.each do |person, contact_details_hash|
