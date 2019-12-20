946fae4ffa5b3aca1c32e09d999c1d6fd790cef6
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-20 09:45:37 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,7 +19,7 @@ def remove_strawberry(contacts)
 contacts.each do |person, contact_details_hash|
   if person =="Freddy Mercury"
     contact_details_hash.each do |attribute, data|
-      if attribute == :
+      if attribute == : :favorite_ice_cream_flavors
   end
 end
 end
